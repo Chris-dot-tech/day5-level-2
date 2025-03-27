23126b8392b18838428bfd9819ae0ab3ab7a84a4
Chris-dot-tech <ephraimchristopher034@gmail.com>
2025-03-27 10:25:16 +0530

    Add files via upload

---
+++ even.js
@@ -0,0 +1,12 @@
+let even = []
+let odd = []
+for(i=1 ; i<=20 ;i++){
+    if ( i%2 != 0) {
+        even.push(i)
+    }
+    else{
+        odd.push(i)
+    }
+}
+console.log( "even No :" , odd);
+console.log( "odd No :" , even );
